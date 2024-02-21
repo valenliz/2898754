5511d709e2f2731f2ad811b441e746e2ff4a41af
Your Name <you@example.com>
2024-02-20 19:04:46 -0500

    commit 4

--- valentina.js
+++ valentina.js
@@ -1,5 +1,5 @@
 
 function saludar() {
-    console.log("¡Hola profe, como estas? ejercicio de git");
+    console.log("¡Hola profe, como estas? ejercicio de git para la primera entrega de evidencias");
 }
 saludar();
